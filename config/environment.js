6f8de434a55c90f83d24b01719827a4338656d6e
Christian <me@rndm.de>
2017-03-28 21:29:23 +0200

    fix(environment): change url, locationType for gh-pages release

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'hal-browser',
     environment: environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: '/hal-browser',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
